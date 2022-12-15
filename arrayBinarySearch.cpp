2feca36094f6eca6e242bde2fee3bd9eeffcdf2d
Prem Kumar <99128172+Premst@users.noreply.github.com>
2022-12-16 02:36:56 +0530

    Add files via upload

---
+++ arrayBinarySearch.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    const int size = 5;
+    int list[size], beg, last, mid, item, i, flag=0;
+    cout<<"Enter 5 array element : ";
+    for(i=0; i<size; i++){
+        cin>>list[i];
+    }
+    cout<<"Displaying array element : ";
+    for(i=0; i<size; i++){
+        cout<<list[i]<<"\t";
+    }
+    cout<<"\nEnter an item for serach : ";
+    cin>>item;
+    beg=0, last=(size-1);
+    while(beg<=last){
+        mid=(beg+last)/2;
+        if(list[mid]==item){
+            flag=1; 
+            break;
+        }
+        if(list[mid]<item){
+            beg = mid+1;
+        }
+        else{
+            last = mid-1;
+        }
+    }
+    if(flag==0){
+        cout<<"Not exist";
+    }
+    else{
+        cout<<"exist";
+    }
+    return 0;
+}
